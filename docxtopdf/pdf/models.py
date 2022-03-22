c8c4b42a12d84ec3288e41a3bc8a481dbe43e840
pv01 <vp@mmvs.ru>
2022-03-22 12:56:17 +0500

    All working!. But has bug with length title

--- docxtopdf/pdf/models.py
+++ docxtopdf/pdf/models.py
@@ -3,9 +3,9 @@ from uuid import uuid4
 # Create your models here.
 class FileDocxPdf(models.Model):
     file_id = models.UUIDField(primary_key=True,default=uuid4,editable=False)
-    type = models.CharField(max_length=255)
-    filename = models.CharField(max_length=255)
-    filepath = models.FilePathField()
+    type = models.CharField(max_length=1024)
+    filename = models.CharField(max_length=1024)
+    filepath = models.FilePathField(max_length=1024)
     filepathpdf = models.FilePathField(default='')
     def save(self, *args,**kwargs):
         super().save(*args, **kwargs)
